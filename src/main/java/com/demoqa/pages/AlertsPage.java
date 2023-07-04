c83c008a7d63962b82bc0606290053d20d84cd22
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-07-04 16:37:13 +0200

    add POM_MavenProject

---
+++ src/main/java/com/demoqa/pages/AlertsPage.java
@@ -0,0 +1,71 @@
+package com.demoqa.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+public class AlertsPage extends BasePage{
+
+    public AlertsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "promtButton")
+    WebElement promtButton;
+
+    public AlertsPage sendMessageToAlert(String message) {
+
+        clickWithJSExecutor(promtButton, 0, 300);
+        if (message != null) {
+            driver.switchTo().alert().sendKeys(message);
+            driver.switchTo().alert().accept();
+        }
+        return this;
+
+    }
+
+    @FindBy(id = "alertButton")
+    WebElement alertButton;
+
+    public AlertsPage clickButtonToSeeAlert() {
+        click(alertButton);
+        driver.switchTo().alert().accept();
+        return this;
+    }
+
+    @FindBy(id = "timerAlertButton")
+    WebElement timerAlertButton;
+
+    public AlertsPage clickButtonToSeeAlertAfterFiveSeconds() {
+        click(timerAlertButton);
+        pause(5000);
+        driver.switchTo().alert().accept();
+        return this;
+    }
+
+    @FindBy(id = "confirmButton")
+    WebElement confirmButton;
+
+    public AlertsPage clickButtonConfirmBoxWillAppear() {
+        clickWithJSExecutor(confirmButton, 0, 300);
+        driver.switchTo().alert().accept();
+        return this;
+    }
+
+    @FindBy(id = "promptResult")
+    WebElement promptResult;
+
+    public AlertsPage assertMessage(String message) {
+        Assert.assertTrue(promptResult.getText().contains(message));
+        return this;
+    }
+
+    @FindBy(id = "confirmResult")
+    WebElement confirmResult;
+
+    public AlertsPage assertMessageConfirmResult(String message) {
+        Assert.assertTrue(confirmResult.getText().contains(message));
+        return this;
+    }
+}
